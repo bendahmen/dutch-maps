12ad625032cb64b10b86ab5556d3d1c83df700ed
Ben Dahmen <bendahmen@gmail.com>
2025-02-05 15:14:44 +0000

    added depositions

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # dutch-maps
-Build a dataset that contains the distance to the next Natura2000 area for each Dutch Gemeente.
+Build a dataset that contains the distance to the next Natura2000 area for each Dutch Gemeente, as well as local deposition levels.
 
 main.py: Read data on Dutch gemeentes and Natura2000 areas and calculate minimum
-distance between each gemeente and Natura2000 area. Then save output
-to a CSV file.
+distance between each gemeente and Natura2000 area, and nitrogen
+deposition levels. Then save output to a CSV file.
 
 Data sources:
     - gemeente_borders.json: 
@@ -12,3 +12,6 @@ Data sources:
     - Natura2000_end2019_shp:
         - Link: https://www.eea.europa.eu/en/datahub/datahubitem-view/6fc8ad2d-195d-40f4-bdec-576e7d1268e4?activeAccordion=1091667,1070049
         - Version: 2019, Apr. 2020, SHP format
+    - geopackage-monitor_2025-01-06:
+        - Link: https://monitor.aerius.nl/monitor/introductie?DATASET=m23&YEAR=2020&ASSESSMENT_AREA=134&BREAKDOWN_TYPE=sector
+        - Location: Link mentioned under 'Introductie/Over Monitor'
--- src/main.py
+++ src/main.py
@@ -1,13 +1,63 @@
 #!/usr/bin/env python
 """
 Read data on Dutch gemeentes and Natura2000 areas and calculate minimum
-distance between each gemeente and Natura2000 area. Then save output
-to a CSV file.
+distance between each gemeente and Natura2000 area, and nitrogen
+deposition levels. Then save output to a CSV file.
 """
 
 import geopandas as gpd
 
 def main():
+    # --- Read data ---
+
+    # read Dutch gemeentes
+    gemeente_borders = gpd.read_file('../data/gemeente_borders.json')
+    # convert to common CRS
+    gemeente_borders = gemeente_borders.to_crs(epsg=28992)
+
+    # read Natura2000 areas
+    natura2000 = gpd.read_file('../data/geopackage-monitor_2025-01-06/natura2000-area-receptor-nitrogen-loads.gpkg', layer='m23')
+    # convert to common CRS
+    natura2000 = natura2000.to_crs(epsg=28992)
+
+    # --- Filter Data ---
+
+    # filter out gemeentes in the Netherlands
+    gemeente_borders = gemeente_borders[gemeente_borders['rubriek']=='gemeente']
+
+    # filter out depositions data from 2020
+    natura2000 = natura2000[natura2000['year']==2020]
+
+    # --- Manipulate Data ---
+
+    # calculate gemeente centroids
+    gemeente_borders['centroid_geometry'] = gemeente_borders.geometry.centroid
+    gemeente_centroids = gpd.GeoDataFrame(
+        gemeente_borders.drop(columns='geometry'), 
+        geometry='centroid_geometry', 
+        crs=gemeente_borders.crs
+    )
+
+    # calculate maximum deposition for each Natura2000 area
+    natura2000['max_deposition'] = natura2000.groupby('natura2000_area_code')['deposition'].transform('max')
+
+    # Use sjoin_nearest to find for each centroid the nearest hexagon from depositions.
+    # We include the distance, deposition and max_deposition columns.
+    nearest = gpd.sjoin_nearest(gemeente_centroids, natura2000[['geometry', 'deposition', 'max_deposition']],how="left",distance_col="distance")
+
+    # --- Save Data ---
+
+    # save to CSV
+    nearest[['statcode', 'statnaam', 'distance', 'deposition', 'max_deposition']].to_csv('../data/output/gemeente_natura2000_distance_and_depositions.csv', index=False)
+    print('Data saved to ../data/output/gemeente_natura2000_distance_and_depositions.csv')
+
+def distances():
+    """
+    Distance between municipalities and Natura2000 areas using
+    official municipality data. The more updated version of this
+    has data on emissions as well.
+    """
+
     # --- Read data ---
 
     # read Dutch gemeentes
@@ -42,7 +92,6 @@ def main():
 
     # save to CSV
     gemeente_borders[['statcode', 'statnaam', 'distance']].to_csv('../data/output/gemeente_natura2000_distance.csv', index=False)
-    print('Data saved to ../data/output/gemeente_natura2000_distance.csv')
 
 if __name__ == '__main__':
     main()
